c9c63551565c91959d68d0492d8d2737d7b8b131
Playingtygre <kymiehsu123@gmail.com>
2018-06-30 16:05:56 -0400

    Adding Model, Controller

---
+++ HelloWorldApplication/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using HelloWorldApplication.Models;
+
+namespace HelloWorldApplication.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        HelloWorldModel[] sayhello = new HelloWorldModel[]
+        {
+            new HelloWorldModel {Name = " Hello World"},
+            new HelloWorldModel {Name = " Hello World"},
+        };
+
+        public IEnumerable<HelloWorldModel> GetHelloWorld()
+        {
+            return sayhello;
+        }
+
+        /* CREATE, UPDATE, AND DELETE METHODS CAN GO HERE
+         */
+
+    }
+}
---
+++ HelloWorldApplication/Models/HelloWorldModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HelloWorldApplication.Models;
+
+namespace HelloWorldApplication.Models
+{
+    public class HelloWorldModel
+    {
+        public string Name { get; set; }
+    };
+}
